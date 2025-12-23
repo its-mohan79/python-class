1820cffab761bf50cc1c8fe31acdb07a5c30a5f5
its-mohan79 <mohanroy8989@gmail.com>
2025-12-23 21:48:46 +0530

    add new my name is mohan

--- module.py
+++ module.py
@@ -10,7 +10,7 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("this is beautiful house  i had a problem so i " \
+engine.say("my name is mohan" \
 "" \
 "" \
 "" \
